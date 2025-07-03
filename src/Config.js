23632091241527d9a09052eeb73de30b32fc78c0
kuldeep kumar <kuldeep.kumar@Fonixtech.io>
2025-07-03 11:35:11 +0530

    url chnage

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://apihair.txogavideo.in/api/v1";
+const BASE_URL = "https://backend.hairsncares.com/api/v1";
 
 // const BASE_URL = "https://v5hqnvqt-5000.inc1.devtunnels.ms/api/v1";
 
