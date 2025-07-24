19137c981b2bcaac89bddb62e6bf860cabb64b05
kuldeep kumar <kuldeep.kumar@Fonixtech.io>
2025-07-24 15:06:03 +0530

    url change

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 5174,
-    allowedHosts: ["https://report.txogavideo.in"]
+    allowedHosts: ["https://report.hairsncares.com"]
   },
 })
